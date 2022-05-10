f5709859351bdd24bc7ba9aa1f2e239ca728374d
Alumno <leo250703@gmail.com>
2022-05-10 17:27:27 -0400

    Commit 10/05/2022 (Leandro Morales) --> Añadimos un comboBox (spinner) y algunas opciones, como la de si seleccionamos faltas, nos generará un random que será el límite ara generar las faltas en base a la serie de fibonacci

--- app/src/main/java/com/example/proyecto21042022/HomeActivity.java
+++ app/src/main/java/com/example/proyecto21042022/HomeActivity.java
@@ -3,15 +3,28 @@ package com.example.proyecto21042022;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private TextView txtResultado;
+    private TextView txtResultado, txtResultadoSP;
+    private Spinner spOpciones;
+    private Button btnCalcular;
 
     private String nombre, apellido, celular, email, codigo;
     private boolean esEstudiante;
     private Estudiante estudiante;
+
+    //En esta estructura vamos a contener los valores de cada opción del combo box
+    private ArrayList<String> opciones;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,70 @@ public class HomeActivity extends AppCompatActivity {
         inicializarVistas();
         recibirDatosPantallaA();
         mostrarDatos();
+        popularSpinner();
+        btnCalcular.setOnClickListener(view -> {
+            obtenerValoresSpinner();
+        });
+    }
+
+    private void obtenerValoresSpinner() {
+        //Información que pueden obtener del elemento
+        //seleccionado en un spinner
+        String valor = spOpciones.getSelectedItem().toString();
+        int posicion = spOpciones.getSelectedItemPosition();
+        switch (valor){
+            case "Notas":
+                //procedimientos
+                break;
+            case "Faltas":
+                //Procedimiento
+                calcularFaltas();
+                break;
+        }
+    }
+
+    private void calcularFaltas() {
+        Random random = new Random();
+        int numRandom = random.nextInt(11);
+        int faltas = 0;
+        int a = 0, b = 1;
+        for (int i = 0; i < numRandom; i++) {
+            a = b;
+            b = faltas;
+            faltas = a + b;
+        }
+
+        if(faltas > 11){
+            txtResultadoSP.setText("Random: " + numRandom +
+                    "\nCantidad de faltas: " + faltas +
+                    "\nEstado: Reprobado");
+        }
+        else{
+            txtResultadoSP.setText("Random: " + numRandom +
+                    "\nCantidad de faltas: " + faltas +
+                    "\nEstado: Aprobado");
+        }
+    }
+
+    private void popularSpinner() {
+        opciones = new ArrayList<>(Arrays.asList("Escoja una opción","Notas","Faltas"));
+        opciones.add("Nueva opción");
+        //Necesitan un adaptador para convertir una fuente de datos primitivos
+        //en un componenete visual que pueda ser desplegado en pantalla.
+        //Adaptador = puente que convierte fuentes de datos en elementos visuales
+        /*
+        Parámetros que necesita un adaptador ya fabricado por defecto
+        en este caso, ese adpatador se denomina ArrayAdapter
+        1: Contexto de la pantalla donde se va a dibujar el componente visual.
+        2: La representación del componente visual o recurso visual que va a dibujar por cada elemento
+        3: Indiquen la fuente de datos, o la estructura que contiene sus datos primitivos
+         */
+        ArrayAdapter<String> adaptador = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_spinner_item,
+                opciones
+                );
+        spOpciones.setAdapter(adaptador);
     }
 
     private void mostrarDatos() {
@@ -33,6 +110,9 @@ public class HomeActivity extends AppCompatActivity {
     private void inicializarVistas() {
         //findViewById solo busca los elementos de la pantalla actual
         txtResultado = findViewById(R.id.txtResultado);
+        spOpciones = findViewById(R.id.spOpciones);
+        btnCalcular = findViewById(R.id.btnCalcular);
+        txtResultadoSP = findViewById(R.id.txtResultadoSP);
     }
 
     private void recibirDatosPantallaA(){
